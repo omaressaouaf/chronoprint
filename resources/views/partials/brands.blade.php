f00fc2399f0d4152d54c77577fa267122a5c91ad
Omar-Ess <souafomar@gmail.com>
2021-12-30 17:36:35 +0100

    refactor statisfied clients section

--- resources/views/partials/brands.blade.php
+++ resources/views/partials/brands.blade.php
@@ -1,12 +1,12 @@
 <section class="container py-lg-4 mb-4">
-    <h2 class="h3 text-center pb-4">{{ __('Satisfied clients') }}</h2>
+    <h2 class="h3 text-center pb-4">{{ __('Satisfied brands') }}</h2>
     <div class="row">
         @foreach (range(1,6) as $item)
             <div class="col-md-4 col-sm-4 col-6">
                 <a class="d-block bg-white shadow-sm rounded-3 py-3 py-sm-4 mb-grid-gutter"
                    href="#">
                     <img class="d-block mx-auto"
-                         src="/storage/theme/shop/brands/0{{$item}}.png"
+                         src="/storage/theme/shop/brands/{{$item}}.png"
                          style="width: 150px;"
                          alt="Brand">
                 </a>
