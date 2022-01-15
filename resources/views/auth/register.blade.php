b666df8e161107ea096af437f9a203fae3c668d4
Omar-Ess <souafomar@gmail.com>
2022-01-15 19:52:36 +0100

    added phone field in registration forms

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -56,6 +56,26 @@
                            @enderror
                         </div>
                      </div>
+                     <div class="row mb-3">
+                        <label for="phone"
+                           class="col-md-4 col-form-label text-md-right">{{ __('Phone') }}</label>
+
+                        <div class="col-md-6">
+                           <input id="phone"
+                              class="form-control @error('phone') is-invalid @enderror"
+                              name="phone"
+                              value="{{ old('phone') }}"
+                              required
+                              autocomplete="phone">
+
+                           @error('phone')
+                              <span class="invalid-feedback"
+                                 role="alert">
+                                 <strong>{{ $message }}</strong>
+                              </span>
+                           @enderror
+                        </div>
+                     </div>
 
                      <div class="row mb-3">
                         <label for="password"
