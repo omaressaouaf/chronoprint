9501ec42648c6524986b8df91267b65be6751cd1
Omar-Ess <souafomar@gmail.com>
2022-05-06 17:44:50 +0000

    make sitemap generation every 2 weeks

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('model:prune')->dailyAt("04:00");
-        $schedule->command('sitemap:generate')->weeklyOn(1 , "04:00");
+        $schedule->command('sitemap:generate')->weeklyOn(2 , "04:00");
     }
 
     /**
