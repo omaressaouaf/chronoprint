1e1d329e8f077832413436247b6b7c0b6fedd28d
Omar-Ess <souafomar@gmail.com>
2022-06-13 17:57:25 +0100

    fix bug in backup config

--- config/database.php
+++ config/database.php
@@ -62,7 +62,7 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
             'dump' => [
-                'dump_binary_path' => 'C:/xampp/mysql/bin/', // only the path, so without `mysqldump` or `pg_dump`
+                'dump_binary_path' => env("MYSQL_DUMP_BINARY_PATH"), // only the path, so without `mysqldump` or `pg_dump`
                 'use_single_transaction',
                 'timeout' => 60 * 5, // 5 minute timeout
             ],
